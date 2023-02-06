c0debd853a58398202b9c653cd11adc79237e45f
ntq-cantv <90816202+ntq-cantv@users.noreply.github.com>
2023-02-06 14:31:18 +0700

    Update MainActivity.kt
    change text main

--- app/src/main/java/com/example/compose_demo1/MainActivity.kt
+++ app/src/main/java/com/example/compose_demo1/MainActivity.kt
@@ -22,7 +22,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Tran van can")
+                    Greeting("Tran van canaaaaa")
                 }
             }
         }
